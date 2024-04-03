7d8114754bad99c15c02b9efc9f0eb9ece4778b1
tomchaccom <134512691+tomchaccom@users.noreply.github.com>
2024-04-03 16:35:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args){
+        
+        int a;
+        Scanner t = new Scanner(System.in);
+        
+        a = t.nextInt();
+        
+        System.out.println(a - 543);
+    
+    }
+   
+
+}
