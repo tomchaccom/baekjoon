808754b17e02b4d3ac67b71f6582d7d50232ca5e
tomchaccom <134512691+tomchaccom@users.noreply.github.com>
2024-04-04 16:46:01 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+        
+        Scanner t = new Scanner(System.in);
+        String a;
+        
+        a = t.next();
+        
+        System.out.println(a+"??!");
+        
+        t.close();
+       
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
