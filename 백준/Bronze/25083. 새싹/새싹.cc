512c32506942feafcbe8bfdae94e0abad3996aba
tomchaccom <134512691+tomchaccom@users.noreply.github.com>
2024-04-09 13:27:42 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    
+    
+    printf("         ,r\'\"7"); 
+    printf("\n");
+    printf("r`-_   ,\'  ,/");
+    printf("\n");
+	printf(" \\. \". L_r\'");
+    printf("\n");
+	printf("   `~\\/");
+    printf("\n");
+	printf("      |");
+    printf("\n");
+	printf("      |");
+    
+    
+    
+  
+    
+    
+    return 0;
+}
