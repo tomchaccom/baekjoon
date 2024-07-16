1067877c2d8c0c528bb690acc1727de5634d7638
myeongsung <134512691+tomchaccom@users.noreply.github.com>
2024-07-17 01:41:41 +0900

    Update 팰린드롬수.java

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,43 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main{
     
     public static void main(String []args){
        
-       Scanner scanner = new Scanner(System.in);
-       List<String> inputs = new ArrayList<>();
-       List<String> results = new ArrayList<>();
-       
-       String input;
-       
-       // 입력을 받아 리스트에 저장
-       while (true) {
-           input = scanner.next();
-           if (input.equals("0")) {
-               break;
-           }
-           inputs.add(input);
+       Scanner t = new Scanner(System.in);
+
+       String fal;
+       StringBuffer str = new StringBuffer();
+       ArrayList<String> list= new ArrayList<>();
+
+    
+       do{
+            fal = t.next();
+            
+            str.append(fal); 
+            str.reverse();
+        
+            if(fal.equals(str.toString()) && !(fal.equals("0"))){
+                list.add("yes");
+            }
+            else{
+                if(!(fal.equals("0"))){
+                    list.add("no");
+                }
+            }
+
+            str.delete(0,fal.length());
+          
+            
+            
+       }while(Integer.parseInt(fal) != 0);
+
+       for(String result:list){
+        System.out.println(result);
        }
-       
-       // 입력 리스트를 처리하고 결과 리스트에 저장
-       for (String fal : inputs) {
-           StringBuffer str = new StringBuffer(fal);
-           str.reverse();
-           
-           if (fal.equals(str.toString())) {
-               results.add("yes");
-           } else {
-               results.add("no");
-           }
-       }
-       
-       // 결과 리스트 출력
-       for (String result : results) {
-           System.out.println(result);
-       }
-       
-       scanner.close();
+
+       t.close();
+
     }
+
+   
 }
