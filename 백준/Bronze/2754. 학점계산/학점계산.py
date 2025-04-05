7563396607e8693ba387a614d9806f7e7f1fd0dd
myeongsung <134512691+tomchaccom@users.noreply.github.com>
2025-04-05 17:53:57 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+n = input()
+
+if "A+" in n:
+    print(4.3)
+elif "A0" in n:
+    print(4.0)
+elif "A-" in n:
+    print(3.7)
+elif "B+" in n:
+    print(3.3)
+elif "B0" in n:
+    print(3.0)
+elif "B-" in n:
+    print(2.7)
+elif "C+" in n:
+    print(2.3)
+elif "C0" in n:
+    print(2.0)
+elif "C-" in n:
+    print(1.7)
+elif "D+" in n:
+    print(1.3)
+elif "D0" in n:
+    print(1.0)
+elif "D-" in n:
+    print(0.7)
+elif "F" in n:
+    print(0.0)
