84f79c5d35840a4f84ad94d2119ce6b966da4b84
myeongsung kim <134512691+tomchaccom@users.noreply.github.com>
2026-01-01 16:16:43 +0900

    [Bronze V] Title: 시험 성적, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,30 +1,24 @@
 import java.util.*;
 
-public class Main{
-    
-    public static void main(String []args){
-        
+public class Main {
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        
         int score = sc.nextInt();
-        
-        if( 90 <= score && score<= 100){
-            sb.append("A");
-        }else if( 80 <= score && score <= 89){
-            sb.append("B");
-        }else if( 70 <= score && score <= 79){
-            sb.append("C");
-        }else if( 60 <= score && score <= 69){
-            sb.append("D");
-        }else{
-            sb.append("F");
+
+        if (90 <= score && score <= 100) {
+            System.out.println("A");
+        } else if (80 <= score && score <= 89) {
+            System.out.println("B");
+        } else if (70 <= score && score <= 79) {
+            System.out.println("C");
+        } else if (60 <= score && score <= 69) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
 
-        System.out.println(sb.toString());
-        
-        
         sc.close();
     }
-    
 }
