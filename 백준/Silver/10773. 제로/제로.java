1ce2cae4f4391d8289551945ae48cb35bebe564b
myeongsung kim <134512691+tomchaccom@users.noreply.github.com>
2026-01-06 13:33:41 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23420 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,32 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-// 리스트를 입력하고 정렬
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 먼저 줄 수를 입력받는다
-
-        int sum = 0;
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine(); // 한줄 입력
+        int k = Integer.parseInt(br.readLine());
+        int num;
+        int sum= 0;
+        int x;
 
-            stack.push(Integer.parseInt(s)); // 입력하고
+        for(int i =0; i < k; i++){
+            num = Integer.parseInt(br.readLine());
 
-            if (stack.peek() == 0){
-                stack.pop(); // 0제거
-                stack.pop(); // 그 앞 수도 제거
+            if(num != 0){
+                stack.add(num);
+                sum += num;
+            }else{
+                x = stack.pop();
+                sum -= x;
             }
-        }
-        for (int j= 0; j < stack.size(); j++) {
-            sum += stack.get(j);
+
         }
 
-        bw.write(sum + "\n");
-        bw.flush();
+        System.out.println(sum);
+
     }
 }
